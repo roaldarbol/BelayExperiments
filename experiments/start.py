be848c40dc924a100e1e87b91f60b1c40f21cac2
roaldarbol <github.jh1q5@passmail.net>
2026-02-05 19:27:15 +0100

    Lots of stuff from the Windows in the workshop

--- experiments/start.py
+++ experiments/start.py
@@ -6,15 +6,8 @@ if project_dir not in sys.path:
 
 import time
 import belay
-from belay import list_devices
-from belay import Device
 from devices.PicoBonn import PicoBonn
 
 # Initialize device
-spec = belay.UsbSpecifier(
-    vid=11914, 
-    pid=5, 
-    serial_number='70973D3DE68AC915', 
-    manufacturer='Microsoft', 
-    location='1-4:x.0')
+spec = belay.UsbSpecifier(vid=11914, pid=5, serial_number='70973D3DE68AC915', manufacturer='Microsoft', location='1-8:x.0')
 pico = PicoBonn(spec)
